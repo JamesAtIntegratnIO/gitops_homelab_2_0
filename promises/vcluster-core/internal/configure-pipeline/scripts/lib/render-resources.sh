5587f23cc12244868316952653ce77bce0415bcd
James D <james@integratn.io>
2026-01-23 16:33:43 -0700

    Allow privileged pod security in vcluster namespaces

--- promises/vcluster-core/internal/configure-pipeline/scripts/lib/render-resources.sh
+++ promises/vcluster-core/internal/configure-pipeline/scripts/lib/render-resources.sh
@@ -9,6 +9,9 @@ metadata:
   labels:
     app: vcluster
     instance: ${NAME}
+    pod-security.kubernetes.io/enforce: privileged
+    pod-security.kubernetes.io/audit: privileged
+    pod-security.kubernetes.io/warn: privileged
 EOF
 }
 
